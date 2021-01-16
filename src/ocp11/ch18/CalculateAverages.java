795c7b1b81d666fcc2b59766763ff8d940fd8994
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2021-01-15 19:09:43 -0500

    Primitive

---
+++ src/ocp11/ch18/CalculateAverages.java
@@ -0,0 +1,14 @@
+package ocp11.ch18;
+
+public class CalculateAverages implements Runnable {
+
+    private double[] scores;
+
+    public CalculateAverages(double[] scores) {
+        this.scores = scores;
+    }
+
+    public void run() {
+// Define work here that uses the scores object
+    }
+}
